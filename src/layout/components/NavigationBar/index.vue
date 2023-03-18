1451bb2734092ba55642eee386cb69b34dab64c5
xiaozhangtongx <2446159171@qq.com>
2023-03-18 15:26:37 +0800

    feat: update NavigationBar

--- src/layout/components/NavigationBar/index.vue
+++ src/layout/components/NavigationBar/index.vue
@@ -53,14 +53,8 @@ const logout = () => {
         </div>
         <template #dropdown>
           <el-dropdown-menu>
-            <a target="_blank" href="https://juejin.cn/post/7089377403717287972">
-              <el-dropdown-item>中文文档</el-dropdown-item>
-            </a>
-            <a target="_blank" href="https://github.com/un-pany/v3-admin-vite">
+            <a target="_blank" href="https://github.com/xiaozhangtongx/v3-admin-vite">
               <el-dropdown-item>GitHub</el-dropdown-item>
-            </a>
-            <a target="_blank" href="https://gitee.com/un-pany/v3-admin-vite">
-              <el-dropdown-item>Gitee</el-dropdown-item>
             </a>
             <el-dropdown-item divided @click="logout">
               <span style="display: block">退出登录</span>
