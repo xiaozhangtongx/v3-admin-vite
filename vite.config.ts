249d6cfa4630990d68fccbefceb1cfbd5e62f2af
xiaozhangtongx <2446159171@qq.com>
2023-03-22 16:40:32 +0800

    fix: fix Cannot find module 'unplugin-vue-define-options/vite'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { type ConfigEnv, type UserConfigExport, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import UnoCSS from 'unocss/vite'
+
+// @ts-expect-error: to resolve Cannot find module 'unplugin-vue-define-options/vite
 import DefineOptions from 'unplugin-vue-define-options/vite'
+
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
